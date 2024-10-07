d35376a3bf4126e80bc587db602ab1d22980d5ad
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2024-10-07 19:15:53 +0300

    alternative client syntax

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -13,6 +13,7 @@ export default [
       'no-unused-vars': 'off',
       'prefer-const': 'off',
       'no-require-imports': 'off',
+      'no-async-promise-executor': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-var-requires': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
